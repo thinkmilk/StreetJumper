106a0202d11e1a765f5d768356b3d26baa641c7d
Liam Relke <lrelke.lr@gmail.com>
2021-04-17 17:34:26 +0900

    Revert "Add project files."
    This reverts commit 2c4d5f7cee49c5572c99f37dc8ef75aa0ec8a8f8.

--- classes/header.ts
+++
@@ -1,4 +0,0 @@
-export * from './actor';
-export * from './player';
-export * from './Alex';
-export * from './pokeplayer';
--- scenes/index.ts
+++
@@ -1,4 +0,0 @@
-// Essentially a header file
-export * from './loading';
-export * from './level-1';
-export * from './world';
